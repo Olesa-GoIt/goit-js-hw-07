dd39ef0d5a32af5db1e29ad27c8dbd3128e876d2
Olesa <cimbalukolesa@gmail.com>
2025-08-30 20:45:12 +0300

    t1.2.3.4.5

---
+++ js/task-4.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // 🚫 відміняємо перезавантаження сторінки
+
+  const { email, password } = form.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  form.reset(); // 🔄 очищаємо форму
+});
